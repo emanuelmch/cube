188890c878091345c89de40c6506f2e42f677e30
Emanuel Machado da Silva <emanuel.mch@gmail.com>
2012-10-12 18:27:14 -0300

    Fix cube's colors

--- src/cube/face.h
+++ src/cube/face.h
@@ -13,18 +13,18 @@ namespace Cube {
 
 	static inline ALLEGRO_COLOR getFaceColor(Face f) {
 		switch (f) {
-		case UP:
-			return al_map_rgb(255, 255, 255);
-		case DOWN:
-			return al_map_rgb(255, 255, 0);
-		case LEFT:
-			return al_map_rgb(124, 252, 0);
-		case RIGHT:
-			return al_map_rgb(0, 0, 255);
-		case FRONT:
-			return al_map_rgb(255, 0, 0);
-		case BACK:
-			return al_map_rgb(255, 165, 0);
+		case UP: // white #FFFFFF
+			return al_map_rgb(0xFF, 0xFF, 0xFF);
+		case DOWN: // yellow #FFD500
+			return al_map_rgb(0xFF, 0xD5, 0x00);
+		case LEFT: // red #C41E3A
+			return al_map_rgb(0xC4, 0x1E, 0x3A);
+		case RIGHT: // orange #FF5800
+			return al_map_rgb(0xFF, 0x58, 0x00);
+		case FRONT: // blue #0051BA
+			return al_map_rgb(0x00, 0x51, 0xBA);
+		case BACK: // green #009E60
+			return al_map_rgb(0x00, 0x9E, 0x60);
 		}
 		return al_map_rgb(0, 0, 0);
 	}
